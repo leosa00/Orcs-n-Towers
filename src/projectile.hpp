8be601dd13d859afc7744cb2f5571830a77a837c
molinell <molinell@arcada.fi>
2023-11-04 20:24:19 +0200

    adds members/methods to Projectile class

--- src/projectile.hpp
+++ src/projectile.hpp
@@ -1,13 +1,26 @@
 #ifndef PROJECTILE
 #define PROJECTILE
 
+#include "tower.hpp"
+#include <SFML/System/Vector2.hpp>
+
 class Projectile
 {
     private:
-    /* data */
+        double speed_;
+        sf::Vector2f velocity_;
+        sf::Vector2f position_;
+        tower* owner_;
+        std::string type_;
+        int damage_;
     public:
         Projectile(/* args */);
         ~Projectile();
+        double getSpeed() const;
+        double getVelocity() const;
+        std::string& getOwner() const;
+        void dealDamage();
+
 };
 
 #endif
