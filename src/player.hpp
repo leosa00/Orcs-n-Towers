7e66d04f10a3777a641e5224b13a49ee9f103482
molinell <molinell@arcada.fi>
2023-11-04 20:23:56 +0200

    adds members/methods to Player class

--- src/player.hpp
+++ src/player.hpp
@@ -1,13 +1,27 @@
 #ifndef PLAYER
 #define PLAYER
 
+#include <string>
+#include <SFML/System/Vector2.hpp>
+
 class Player
 {
     private:
-    /* data */
+        int hp_;
+        int wallet_;
+        std::string name_;
+        int score_;
+        sf::Vector2f position_;
     public:
         Player();
         ~Player();
+        int getWallet() const;
+        int getHP() const;
+        std::string& getName() const;
+        void addMoney();
+        void removeMoney();
+        void removeHP();
+
 };
 
 #endif
