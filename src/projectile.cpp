2815ddda0447b9309fb152cca9d8e8fb20da55ec
molinell <molinell@arcada.fi>
2023-11-07 13:19:41 +0200

    defines methods for projectile class

--- src/projectile.cpp
+++ src/projectile.cpp
@@ -1,1 +1,21 @@
 #include "projectile.hpp"
+
+double Projectile::getSpeed() const {
+    return speed_;
+}
+
+sf::Vector2f Projectile::getVelocity() const {
+    return velocity_,;
+}
+
+Tower* Projectile::getOwner() const{
+    return owner_;
+}
+
+std::string& getType() const {
+    return type_;
+}
+
+void Projectile::dealDamage(Enemy* enemy){
+    //get access to enemies hp and reduct projectiles damage
+}
--- src/projectile.hpp
+++ src/projectile.hpp
@@ -2,7 +2,7 @@
 #define PROJECTILE
 
 #include "tower.hpp"
-#include <SFML/System/Vector2.hpp>
+//#include <SFML/System/Vector2.hpp>
 
 class Projectile
 {
@@ -10,16 +10,17 @@ class Projectile
         double speed_;
         sf::Vector2f velocity_;
         sf::Vector2f position_;
-        tower* owner_;
+        Tower* owner_;
         std::string type_;
         int damage_;
     public:
-        Projectile(/* args */);
-        ~Projectile();
+        Projectile(Tower* owner) : speed_(), velocity_(), position_(), owner_(owner), type_(), damage_(){}
+        ~Projectile() {}
         double getSpeed() const;
-        double getVelocity() const;
-        std::string& getOwner() const;
-        void dealDamage();
+        sf::Vector2f getVelocity() const;
+        Tower* getOwner() const;
+        std::string& getType() const;
+        void dealDamage(Enemy* enemy);
 
 };
 
