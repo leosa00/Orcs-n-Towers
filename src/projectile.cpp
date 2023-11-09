d7cc78ce6ba691479cc5f849af760b9fe5d2b8b7
molinell <molinell@arcada.fi>
2023-11-09 18:09:02 +0200

    calcs distance from projectile to tower

--- src/projectile.cpp
+++ src/projectile.cpp
@@ -1,4 +1,5 @@
 #include "projectile.hpp"
+#include <cmath>
 
 double Projectile::getSpeed() const {
     return speed_;
@@ -12,10 +13,22 @@ Tower* Projectile::getOwner() const{
     return owner_;
 }
 
-std::string& getType() const {
+std::string& Projectile::getType() const {
     return type_;
 }
 
-void Projectile::dealDamage(Enemy* enemy){
+void Projectile::dealDamage(Enemy& enemy){
     //get access to enemies hp and reduct projectiles damage
+}
+
+void Projectile::distToTower() {
+    sf::Vector2f pPos = this.getPosition(); //function from transformable class
+    sf::Vector2f tPos = owner_.getPosition();
+
+    double dist = sqrt(pow((tPos.x - pPos.x),2) + pow((tPos.y - pPos.y),2))
+
+    if(dist > 10) //need to determine some reasonable distance from the tower
+    {
+        delete this;
+    }
 }
--- src/projectile.hpp
+++ src/projectile.hpp
@@ -2,9 +2,10 @@
 #define PROJECTILE
 
 #include "tower.hpp"
-//#include <SFML/System/Vector2.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Transformable.hpp>
 
-class Projectile
+class Projectile : public sf::Transformable
 {
     private:
         double speed_;
@@ -13,15 +14,22 @@ class Projectile
         Tower* owner_;
         std::string type_;
         int damage_;
+
     public:
-        Projectile(Tower* owner) : speed_(), velocity_(), position_(), owner_(owner), type_(), damage_(){}
+        Projectile(double speed, sf::Vector2f velocity, sf::Vector2f position, Tower* owner, std::string type, int damage) : speed_(speed), velocity_(velocity), position_(position), owner_(owner), type_(type), damage_(damage){}
         ~Projectile() {}
         double getSpeed() const;
         sf::Vector2f getVelocity() const;
         Tower* getOwner() const;
         std::string& getType() const;
-        void dealDamage(Enemy* enemy);
+        void dealDamage(Enemy& enemy);
 
+        /**
+         * calculates distance from owner tower
+         * deletes the projectile if it hasn't hit an enemy and is just travelling further from tower
+         * so that projectiles don't just float about when they've gone out of range
+         * probably needs to be called every frame after the projectile has been shot
+        */
+        void distToTower(); 
 };
-
 #endif
