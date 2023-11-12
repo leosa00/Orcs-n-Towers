3c7a3c1d78e99996cd01d6cbfabc59e426869f0f
tweetyducky <tweety@Leos-PC>
2023-11-12 15:10:43 +0200

    changes to enemy.hpp

--- src/enemy.hpp
+++ src/enemy.hpp
@@ -1,14 +1,24 @@
+#ifndef ENEMY_HPP
+#define ENEMY_HPP
 #include <string>
-
-
-class Enemy {
+#include <queue>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics.hpp> 
+class Enemy :public sf::Drawable {
 public:
-    Enemy(int hp, int speed, std::string type)
-        : hp_(hp), speed_(speed), type_(type) {}
+    Enemy(int hp, int speed, std::string type, int gold)
+        : hp_(hp), speed_(speed), type_(type), goldWorth_(gold) {}
 
     ~Enemy() {}
 
-    void Move(); //use the speed_ variable to advance the position of the enemy, keep it void for now
+    void move(); //use the speed_ variable to advance the position of the enemy, keep it void for now
+    
+    void update(); //update the state of the monster in relation to the game
+
+    bool atBase(); //checks if the enemy has reached the base
+
+    sf::Vector2f getPosition(); //returns the position of the enemy as a Vector2f position
+
     
     int hp();
     
@@ -37,5 +47,15 @@ private:
     // the length of time that the enemy is poisoned for depends on how large the poison
     //value is as the number decreases incrimently until 0
     int slowed_; 
-    //state member needs to be implemented
+    //How much gold the player recieves for killing the monster
+    int goldWorth_;
+    //waypoint based movement, the map class should provide a vector of waypoints that take the enemies through the path to the end
+    sf::Vector2f velocity_;
+	std::queue<sf::Vector2f> waypoints_;
+	sf::Vector2f *currentWaypoint_;
+    //sprite
+    sf::Sprite sprite_;
+    sf::Vector2u spriteSize_;
 };
+
+#endif
