e74c3515c967b0d118a59f4fece2a07defc71813
Saied Ahmad Leo <leo.saiedahmad@aalto.fi>
2023-11-20 13:58:41 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 8f327b4e8b7238d592b6936ac44356cd619e7b64

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "game.hpp"
+#include "tower.hpp"
+#include "enemy.hpp"
+#include "projectile.hpp"
+
+int main() {
+    Game game;
+    game.run();
+    return 0;
+}
--- src/projectile.cpp
+++ src/projectile.cpp
@@ -21,11 +21,7 @@ int Projectile::getDamage() const {
     return damage_;
 }
 
-/*void Projectile::dealDamage(Enemy& enemy){
-    //get access to enemies hp and reduct projectiles damage
-}*/
-
-void Projectile::distToTower() {
+bool Projectile::distToTower() {
     sf::Vector2f pPos = this.getPosition(); //function from transformable class
     sf::Vector2f tPos = owner_.getPosition();
 
@@ -34,21 +30,24 @@ void Projectile::distToTower() {
     if(dist > 10) //need to determine some reasonable distance from the tower
     {
         delete this;
+        return true;
     }
+    return false;
 }
 
-bool Projectile::collision(Enemy& enemy) {
+bool Projectile::collision() {
 
-    if(this.getGlobalBounds().intersects(enemy.getGlobalBounds())){
-        enemy.takeDamage(this.getDamage());
+    if(this.getGlobalBounds().intersects(targetEnemy_.getGlobalBounds())){
+        targetEnemy_.takeDamage(this.getDamage());
+        //Player::addToWallet() //send enemy worth as param
         delete this;
         return true;
     }
     return false;
 }
 
-sf::Vector2f Projectile::shootDirection(Enemy& enemy) {
-    sf::Vector2f direction = enemy.getPosition() - position_;
+/*void Projectile::shootDirection() {
+    sf::Vector2f direction = targetEnemy_.getPosition() - position_;
 
     //need to normalise the directional vector to control the speed
     float len = sqrt(pow(direction.x,2)+ pow(direction.y,2));
@@ -57,7 +56,21 @@ sf::Vector2f Projectile::shootDirection(Enemy& enemy) {
         direction.y = direction.y / len;
     }
 
-    direction *= velocity_
+    direction *= velocity_;
 
-    return direction;
+    shootDirection_ = direction;
+}
+
+sf::Vector2f Projectile::getDirection(){
+    return shootDirection_;
+}*/
+
+void Projectile::update(float dt){
+    //only move the projectile if it hasn't collided or gone out of range
+    if(!collision()){
+        if(!distToTower()){
+            this.move(shootDirection_.x * speed_ * dt, shootDirection_.y * speed_ * dt);
+            //shootDirection dictates towards where, speed how fast, dt makes it consistent across framerates
+        }
+    }
 }
