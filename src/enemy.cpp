9c4a2a0eeb1c7e993cb9b2d5be62fb4f360a59c3
Leo <leo.saiedahmad@aalto.fi>
2023-11-23 17:25:53 +0200

    enemy test

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,47 +1,36 @@
 #include "enemy.hpp"
 #include <string>
-#include <cmath>
 
-// Sprite has move with essentially this exact implementation
-//void Enemy::move(sf::vector2f movement){//Implement a function that allows the sprite to move
-////update the state of the enemy with relation to the game
-//    sprite.move(movement);
-//    //add more implementations for moving other textures
-//}
+Enemy::Enemy()
 
+
+void Enemy::move(sf::vector2f movement){//Implement a function that allows the sprite to move
+//update the state of the enemy with relation to the game
+    sprite.move(movement);
+    //add more implementations for moving other textures
+}
 void Enemy::update() {
 //game based reference to the time, use this with velocity
 //use movement function to move the enemy taking the distance 
 //moved based on time and velocity calculation 
-    float timeVariable; //this is just a placeholder varaible for the time
+    float timeVariable //this is just a placeholder varaible for the time
 //variable derived from the game instance
     sf::Vector2f movement = velocity_ * timeVariable; //enemy velocity variable
     //is multiplied by the derived time variable
     //will add a way to change movement based on if the character is slowed
-    move(movement);
+    move(movement)
 
 }
-
-sf::Vector2f Enemy::getCenter()
-{
-	sf::Vector2f enemyCenter;
-	enemyCenter.x = getPosition().x + getGlobalBounds().width / 2;
-	enemyCenter.y = getPosition().y + getGlobalBounds().height / 2;
-
-	return enemyCenter;
-}
-
-
 //checks if the current way point has been passed, returns trur
 //if it has otherwise false
-bool Enemy::isWaypointPassed(sf::Vector2f movement) {
+bool isWaypointPassed(sf::Vector2f movement) {
 
-    float currentDistance = std::abs(getCenter().x - currentWaypoint_->x) + 
-		fabs(getCenter().y - currentWaypoint_->y);
+    float currentDistance = fabs(getCenter().x - currentWaypoint->x) + 
+		fabs(getCenter().y - currentWaypoint->y);
     
 
-    float nextDistance = fabs(getCenter().x + movement.x - currentWaypoint_->x) +
-		fabs(getCenter().y + movement.y - currentWaypoint_->y);
+    float nextDistance = fabs(getCenter().x + movement.x - currentWaypoint->x) +
+		fabs(getCenter().y + movement.y - currentWaypoint->y);
 
     if (nextDistance < currentDistance) {
         return false;
@@ -50,63 +39,68 @@ bool Enemy::isWaypointPassed(sf::Vector2f movement) {
     }
 }
 
-// Sprite has setTexture
 //void Enemy::setTexture(sf::Texture * texture)
 //{
 //	sprite_.setTexture(*texture);
 //}
 
-// Sprite handles drawing
 //void Enemy::draw(sf::RenderTarget & target) const {
 //    target.draw(sprite_);
 //}
 
-// Sprite has getPosition()
 //sf::Vector2f Enemy::getLocation() {
 //    return sprite_.getPosition();
 //}
 
-// Sprite has getGlobalBounds()
 //sf::FloatRect Enemy::getGlobalBounds()
 //{
 //	return sprite_.getGlobalBounds();
 //}
 
+sf::Vector2f Enemy::getCenter()
+{
+	sf::Vector2f enemyCenter;
+	center.x = sprite_.getPosition().x + sprite_.getGlobalBounds().width / 2;
+	center.y = sprite_.getPosition().y + sprite_.getGlobalBounds().height / 2;
+
+	return enemyCenter;
+}
+
 void Enemy::setVelocity() {
 
     sf::Vector2f distance;
-	distance = *currentWaypoint_ - getCenter();
+	distance = *currentWaypoint - getCenter();
 
-	velocity_.x = distance.x * speed_ / fabs(distance.x + distance.y);
-	velocity_.y = distance.y * speed_ / fabs(distance.x + distance.y);
+	velocity.x = distance.x * speed_ / fabs(distance.x + distance.y);
+	velocity.y = distance.y * speed_ / fabs(distance.x + distance.y);
 
-	if (fabs(velocity_.x) > fabs(velocity_.y))
+	if (fabs(velocity.x) > fabs(velocity.y))
 	{
-		if (velocity_.x > 0)
+		if (isPositive(velocity.x))
 		{
-			velocity_.x = speed_;
-			velocity_.y = 0.f;
+			velocity.x = speed_;
+			velocity.y = 0.f;
 			direction_ = 2;
 		}
 		else
 		{
-			velocity_.x = -speed_;
-			velocity_.y = 0.f;
+			velocity.x = -speed_;
+			velocity.y = 0.f;
 			direction_ = 1;
 		}
 	}
 	else
 	{
-		if (velocity_.y > 0)
+		if (isPositive(velocity.y))
 		{
-			velocity_.x = 0.f;
-			velocity_.y = speed_;
+			velocity.x = 0.f;
+			velocity.y = speed_;
 			direction_ = 0;
 		}
 		else
 		{
-			velocity_.x = 0.f;
-			velocity_.y = -speed_;
+			velocity.x = 0.f;
+			velocity.y = -speed_;
 			direction_ = 3;
 		}
 	}
@@ -121,11 +115,7 @@ int Enemy::speed() {
     return speed_;
 }
 
-/*type Enemy::type() {
-    return type_;
-}*/
-
-EnemyType Enemy::type() {
+std::string Enemy::type() {
     return type_;
 }
 
