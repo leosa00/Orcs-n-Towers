22512735002fce47fb7e0a08734ee5c6570f3cf2
Pavel Filippov <pavel.filippov@aalto.fi>
2023-11-26 13:59:46 +0200

    No changes

--- src/projectile.hpp
+++ src/projectile.hpp
@@ -8,44 +8,50 @@
 #include <SFML/Graphics/Transformable.hpp>
 #include <SFML/Graphics.hpp>
 
+class Game;
+
 // Removed inheritance of sf::Transformable. 
 // I checked that sf::Sprite inherits both Drawable and Transformable -Otto
 class Projectile : public sf::Sprite
 {
     private:
         float speed_;
-        Tower& owner_;
         std::string type_;
         int damage_;
-        sf::Vector2f position_;
+        sf::Vector2f position_; // of tower that created
         int maxDistance_;
         sf::Vector2f shootDirection_;
+        bool isDestroyed_;
 
         //derived classes keep track of their target enemy/ies
 
     public:
         //derived classes should have default speed, type, damage
-        //shootdirection, position, owner, comes from tower
+        
         //Pavel: I assumed you want normalized shootdirection when passing this
         //argument to Projectile constructor inside Tower::shoot().
         //I also commented out my alternative implementation for Projectile constructor below:
         //I think it should be preferred as it makes it more clear which initializers are default
         //and which are passed as an argument from Tower::shoot() method. 
 
-        Projectile(sf::Vector2f shootDirection, sf::Vector2f position, Tower& owner, float speed = 1.0, std::string type = "default", int damage = 0, int maxDistance = 0) 
-        : shootDirection_(shootDirection), position_(position), owner_(owner), speed_(speed), type_(type), damage_(damage), maxDistance_(maxDistance){}
+        //abstract class, don't call this constructor
+        Projectile(sf::Vector2f shootDirection, sf::Vector2f position, int damage, float speed, std::string type, int maxDistance) 
+        : shootDirection_(shootDirection), position_(position), damage_(damage), speed_(speed), type_(type), maxDistance_(maxDistance),
+        isDestroyed_(false){}
 
-        /*Projectile(sf::Vector2f shootDirection, sf::Vector2f position, Tower& owner, int damage) 
-        : shootDirection_(shootDirection), position_(position), owner_(owner), damage_(damage), speed_(1.0), type_("default"), maxDistance_(0) {}*/
+        // shootdirection, position, damage comes from tower
+        // speed, type, maxDistance come from derived classes 
+        // shootDirection needs to be normalised vector
         
-        ~Projectile() {}
+        virtual ~Projectile() {}
         
         float getSpeed() const;
-        Tower& getOwner() const;
         std::string& getType() const;
         int getDamage() const;
         sf::Vector2f getShootDir() const;
         sf::Vector2f getVelocity() const;
+        void destroy();
+        bool isDestroyed();
 
         /**
          * calculates distance from owner tower
@@ -61,7 +67,7 @@ class Projectile : public sf::Sprite
          * deals damage to enemy
          * deletes the projectile
         */
-        virtual bool hasHitEnemy(std::shared_ptr<Enemy> enemy) = 0;
+        virtual bool hasHitEnemy(Enemy& enemy) = 0;
 
         /**
          * moves the projectile if it hasn't collided / gone out of range
@@ -69,6 +75,7 @@ class Projectile : public sf::Sprite
          * towers should call update on their projectiles?
          * purely virtual
         */
-        virtual void update(float dt) = 0;
+       virtual void update(Game&) = 0;
 };
 #endif
+
