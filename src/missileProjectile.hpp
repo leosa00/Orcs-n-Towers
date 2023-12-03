c61321b686a65fb757a9e0682bebcb1a520a2d32
Pavel Filippov <pavel.filippov@aalto.fi>
2023-12-03 17:56:31 +0200

    added comment to MissileProjectile.hpp

--- src/missileProjectile.hpp
+++ src/missileProjectile.hpp
@@ -9,7 +9,8 @@ private:
     std::shared_ptr<Enemy>& targetEnemy_;
 
 public:
-    MissileProjectile(sf::Vector2f position, int damage, std::shared_ptr<Enemy>&& targetEnemy) //modified std::shared_ptr<Enemy>& targetEnemy to std::shared_ptr<Enemy>&& targetEnemy (std::shared_ptr<Enemy>& targetEnemy was giving error in shoot method)
+    //modified std::shared_ptr<Enemy>& targetEnemy to std::shared_ptr<Enemy>&& targetEnemy (std::shared_ptr<Enemy>& targetEnemy was giving error in shoot method)
+    MissileProjectile(sf::Vector2f position, int damage, std::shared_ptr<Enemy>&& targetEnemy) 
     : Projectile(sf::Vector2f(0,0), position, damage, 70.0, "missile", 70), targetEnemy_(targetEnemy) {}
 
     bool hasHitEnemy(std::shared_ptr<Enemy>& enemy);
