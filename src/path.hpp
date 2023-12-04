7a375a2d5425b2b00a5c3d0eb2e11a424fa85332
tuanvukng01 <149362903+tuanvukng01@users.noreply.github.com>
2023-12-04 16:53:30 +0200

    Add Path, partly implement function so that player cannot build tower in the path.

--- src/path.hpp
+++ src/path.hpp
@@ -2,7 +2,8 @@
 #define PATH_HPP
 #include <queue>
 #include <SFML/System/Vector2.hpp>
-#include <SFML/Graphics.hpp> 
+#include <SFML/Graphics.hpp>
+#include <vector>
 
 class path {
     friend class enemy;
@@ -10,16 +11,22 @@ public:
     path();//creates a path and populates the waypoints queue with
     //all the waypoints required for the enemy class to traverse the path
 
-    
+
     ~path() {
-        
+
     }
 
     void addWaypoint(const sf::Vector2f& point);
-    
+
     std::queue<sf::Vector2f> getWaypoints() const;
-private:
+    void makeUnBuildablePath();
+    static const float width;
     std::queue<sf::Vector2f> waypoints_;
+    std::vector <sf::Vector2f> wayPoints;
+    std::vector <sf::FloatRect> unBuildable;
+
+private:
+
 };
 
 #endif
