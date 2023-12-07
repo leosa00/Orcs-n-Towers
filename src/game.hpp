987051cfe160ce5128dc53ffaba0f4a76c0b187a
Otto Litkey <otto.litkey@aalto.fi>
2023-12-08 00:42:17 +0200

    Explosions. They are ugly.

--- src/game.hpp
+++ src/game.hpp
@@ -17,6 +17,7 @@
 #include "menu.hpp"
 #include <vector>
 #include "levelManager.hpp"
+#include "explosion.hpp"
 
 class Menu;
 // Class for running the game logic
@@ -92,6 +93,7 @@ private:
         */
     std::list<std::shared_ptr<Enemy>> enemies_;
     std::list<Projectile*> projectiles_;
+    std::list<Explosion*> explosions_;
     path path_;
     std::list<Button> buttons_; // Stores clickable buttons
     bool dragged_; // Indicates if a tower is currently being dragged into place
