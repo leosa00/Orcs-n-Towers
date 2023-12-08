f3e9bce866003c2d5b0ae939354b79957d6bef9f
molinell <molinell@arcada.fi>
2023-12-08 10:11:07 +0200

    faster missile, more money for player

--- src/missileProjectile.hpp
+++ src/missileProjectile.hpp
@@ -19,7 +19,7 @@ public:
      * @param targetEnemy is the enemy that the missile is targeting (following)
     */
     MissileProjectile(sf::Vector2f position, int damage, std::shared_ptr<Enemy> targetEnemy) 
-    : Projectile(sf::Vector2f(0,0), position, damage, 70.0, "missile", 70), targetEnemy_(targetEnemy) {}
+    : Projectile(sf::Vector2f(0,0), position, damage, 100.0, "missile", 70), targetEnemy_(targetEnemy) {}
 
     /**
      * Checks whether the missile has hit its target or not.
