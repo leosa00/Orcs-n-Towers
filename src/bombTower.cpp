27bcc0d62d1d42d37e90fe73d8c608728cdce64f
Pavel Filippov <pavel.filippov@aalto.fi>
2023-12-10 17:10:35 +0200

    Added comments for Bullet Tower and Bomb Tower

--- src/bombTower.cpp
+++ src/bombTower.cpp
@@ -4,12 +4,23 @@
 #include <cmath>
 #include <memory>
 #include <iostream>
-
+/**
+ * @brief Constructs a new Bomb Tower object
+ * 
+ * @param position Mouse position passed by the caller.
+ * 
+ * Uses base Tower constructor.
+ */
 BombTower::BombTower(sf::Vector2f position)
     : Tower(position, "Bomb Tower", 300, 200.0f, sf::seconds(1.5), 15, 1, 450, nullptr, false) {}
                             //tbd    ^   ^      ^     ^       ^
-//since BombTower can only lock on Ground units, update() method has to be overriden
-
+/**
+ * 
+ * This override for update() is very similar to update() method of base Tower class. 
+ * The only difference is that it also checks EnemyType of an enemy as Bomb Towers can only lock on 
+ * enemies of EnemyType::Ground.
+ * 
+ */
 void BombTower::update(std::list<std::shared_ptr<Enemy>> &enemies, sf::Time time) {
     updateFireTimer(time);
     auto lockedEnemy = getLockedEnemy();
@@ -25,10 +36,17 @@ void BombTower::update(std::list<std::shared_ptr<Enemy>> &enemies, sf::Time time
         }
     }   
 }
+/**
+ * 
+ * shoot() method calculates the direction towards locked enemy, normalizes it, and creates
+ * a BulletProjectile that takes normalized direction, tower's position, damage, and 
+ * locking range of the tower as arguments. 
+ * 
+ */
 BombProjectile* BombTower::shoot() {
     sf::Vector2f direction = getLockedEnemy()->getPosition() - getPosition();
     float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
     sf::Vector2f normalizedDirection = direction / length;
-    BombProjectile* bombProjectile = new BombProjectile(normalizedDirection, getPosition(), getDamage(), getRange()/2);
+    BombProjectile* bombProjectile = new BombProjectile(normalizedDirection, getPosition(), getDamage(), getRange());
     return bombProjectile;    
 }
--- src/bulletTower.hpp
+++ src/bulletTower.hpp
@@ -2,9 +2,26 @@
 #define BULLET_TOWER_H
 #include "tower.hpp"
 #include "bulletProjectile.hpp"
+/**
+ * @class Bullet Tower
+ * @brief Represents the Bullet Tower class.
+ * 
+ * The Bullet Tower is a specialized tower that shoots bullet projectiles. It is derived from
+ * the base Tower class and inherits common tower functionalities.
+ */
 class BulletTower : public Tower {
 public:
-    BulletTower(sf::Vector2f);
+    /** 
+     * @brief Constructs a BulletTower object at the specified position.
+     * 
+     * @param position The initial position of the BulletTower.
+    */
+    BulletTower(sf::Vector2f position);
+    /**
+     * @brief Override of the base class method to produce a BulletProjectile.
+     * 
+     * @return BulletProjectile* A pointer to the created BulletProjectile object.
+     */
     BulletProjectile* shoot() override;
 };
 #endif //BULLET_TOWER
